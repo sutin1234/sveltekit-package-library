e4d37ffec95d41b8fd65629094bf2f34deb906c1
Sutin <poly.dev2018@gmail.com>
2021-11-28 22:44:34 +0700

    added sveltekit package

---
+++ src/lib/SveltePassword.svelte
@@ -0,0 +1,1 @@
+<h3>SveltePassword</h3>
---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script context="module" lang="ts">
+	import SveltePassword from '$lib/SveltePassword.svelte';
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<SveltePassword />
